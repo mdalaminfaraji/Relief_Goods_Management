6015c55535a6af94bcce6f413dc883050e4d23cc
alaminfaraji <alaminice1617@gmail.com>
2024-03-20 03:15:28 +0600

    create about us page

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -1,4 +1,6 @@
+import { Card, Divider, Table } from "antd";
 import { useGetAllVolunteerQuery } from "../../redux/features/volunteer/volunteerApi";
+import Title from "antd/es/typography/Title";
 
 const AboutUs = () => {
   const { data, isLoading, isError } = useGetAllVolunteerQuery(undefined);
@@ -8,8 +10,51 @@ const AboutUs = () => {
   if (isError) {
     return <p>Something Went wrong.........</p>;
   }
-  console.log(data);
-  return <div>AboutUs</div>;
+  const columns = [
+    {
+      title: "Image",
+      dataIndex: "imageUrl",
+      key: "imageUrl",
+      render: (imageUrl: string | undefined) => (
+        <img
+          src={imageUrl}
+          alt="User"
+          style={{ width: 50, height: 50, borderRadius: "50%" }}
+        />
+      ),
+    },
+    {
+      title: "Full Name",
+      dataIndex: "fullName",
+      key: "fullName",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Location",
+      dataIndex: "location",
+      key: "location",
+    },
+    {
+      title: "Phone Number",
+      dataIndex: "phoneNumber",
+      key: "phoneNumber",
+    },
+  ];
+  return (
+    <Card style={{ borderRadius: "0px" }}>
+      <Title style={{ textAlign: "center" }}>Our Volunteers</Title>
+      <Divider>__</Divider>
+      <Table
+        dataSource={data?.data}
+        columns={columns}
+        style={{ maxWidth: "1080px", margin: "0 auto" }}
+      />
+    </Card>
+  );
 };
 
 export default AboutUs;
